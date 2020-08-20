b2afc48eee8a8dabf180be759b8b7d91ac2478a0
sru4607 <sru4607@g.rit.edu>
2020-08-20 15:26:21 -0400

    completed the steps provided in the exercise upto confirming that it works with CircleCI

---
+++ src/imageResponses.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const spongegar = fs.readFileSync('$__dirname}/../client/spongegar.png');
+
+const getSpongeGar = (request, response) => {
+  response.writeHead(200, { Content_Type: 'image/png' });
+  response.write(spongegar);
+  response.end();
+};
+
+module.exports.getSpongeGar = getSpongeGar;
